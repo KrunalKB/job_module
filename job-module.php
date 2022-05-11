586a7a2aac93f3e660cd241b01efcc3d1f5dd81b
Krunal-KB <krunalbhimajiyani11@gmail.com>
2022-05-12 01:47:11 +0530

    initial commit

---
+++ job-module.php
@@ -0,0 +1,13 @@
+<?php
+/**
+ * Plugin Name:       Job module
+ * Description:       This is job module for client and contractor.
+ * Version:           1.0.0
+ * Requires at least: 5.8
+ * Requires PHP:      7.0
+ * Author:            Krunal Bhimajiyani
+ * Author URI:        https://github.com/KrunalKB
+ * License:           GPL-2.0-or-later
+ * License URI:       https://www.gnu.org/licenses/gpl-2.0.html
+ */
+ 
