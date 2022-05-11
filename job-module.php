409b346932c3dd7136f2d61a8ff66ed98f8bd0a1
Krunal-KB <krunalbhimajiyani11@gmail.com>
2022-05-12 03:36:59 +0530

    initial comit

--- job-module.php
+++ job-module.php
@@ -10,4 +10,21 @@
  * License:           GPL-2.0-or-later
  * License URI:       https://www.gnu.org/licenses/gpl-2.0.html
  */
- 
+
+ if (!defined('ABSPATH')) {
+     exit;
+ }
+
+class jbm_controller
+{
+    public function __construct()
+    {
+        register_activation_hook(__FILE__, array($this,'jbm_activate'));
+    }
+
+    public function jbm_activate()
+    {
+    }
+}
+
+$jbm_controller = new jbm_controller();
