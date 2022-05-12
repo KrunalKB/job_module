7666d0d5c03b036f36e792318bde34f8dc6a52b7
Krunal Bhimajiyani <krunalbhimajiyani11@gmail.com>
2022-05-12 19:15:05 +0530

    commit

---
+++ inc/contractor-regitration.php
@@ -0,0 +1,131 @@
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="UTF-8" />
+    <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <title>Registration Form</title>
+    <style>
+      body {
+        background-color: black;
+      }
+
+      * {
+        box-sizing: border-box;
+      }
+
+      /* Add padding to containers */
+      .container {
+        padding: 16px;
+        background-color: white;
+      }
+
+      /* Full-width input fields */
+      input {
+        width: 100%;
+        padding: 15px;
+        margin: 5px 0 22px 0;
+        display: inline-block;
+        border: none;
+        background: #f1f1f1;
+      }
+
+      /* Overwrite default styles of hr */
+      hr {
+        border: 1px solid #f1f1f1;
+        margin-bottom: 25px;
+      }
+
+      /* Set a style for the submit button */
+      .registerbtn {
+        background-color: #04aa6d;
+        color: white;
+        padding: 16px 20px;
+        margin: 8px 0;
+        border: none;
+        cursor: pointer;
+        width: 100%;
+        opacity: 0.9;
+      }
+
+      .registerbtn:hover {
+        opacity: 1;
+      }
+    </style>
+  </head>
+  <body>
+    <form action="">
+      <div class="container">
+        <p>Please fill in this form to create an account.</p>
+        <hr />
+
+        <label for="username"><b>Username</b></label>
+        <input
+          type="text"
+          placeholder="Enter username"
+          name="username"
+          id="username"
+          required
+        />
+
+        <label for="email"><b>Email</b></label>
+        <input
+          type="email"
+          placeholder="Enter Email"
+          name="email"
+          id="email"
+          required
+        />
+
+        <label for="fname"><b>First name</b></label>
+        <input
+          type="text"
+          placeholder="Enter first name"
+          name="fname"
+          id="fname"
+          required
+        />
+
+        <label for="lname"><b>Last name</b></label>
+        <input
+          type="text"
+          placeholder="Enter last name"
+          name="lname"
+          id="lname"
+          required
+        />
+
+        <label for="psw"><b>Password</b></label>
+        <input
+          type="password"
+          placeholder="Enter Password"
+          name="psw"
+          id="psw"
+          required
+        />
+
+        <label for="buss_name"><b>Bussiness name</b></label>
+        <input
+          type="text"
+          placeholder="Enter Bussiness name"
+          name="buss_name"
+          id="buss_name"
+          required
+        />
+
+        <label for="buss_phone"><b>Bussiness number</b></label>
+        <input
+          type="text"
+          placeholder="Enter Bussiness number"
+          name="buss_phone"
+          id="buss_phone"
+          required
+        />
+
+        <hr />
+
+        <button type="submit" class="registerbtn">Register</button>
+      </div>
+    </form>
+  </body>
+</html>
--- job-module.php
+++ job-module.php
@@ -1,6 +1,6 @@
 <?php
 /**
- * Plugin Name:       Job module
+ * Plugin Name:       Job Module
  * Description:       This is job module for client and contractor.
  * Version:           1.0.0
  * Requires at least: 5.8
@@ -11,19 +11,103 @@
  * License URI:       https://www.gnu.org/licenses/gpl-2.0.html
  */
 
- if (!defined('ABSPATH')) {
-     exit;
- }
+if (!defined('ABSPATH')) {
+    // Exit if accessed directly.
+
+    exit;
+}
+
+/**
+ *  Class jbm_controller
+ */
 
 class jbm_controller
 {
+    /**
+     * Construct function
+     */
+
     public function __construct()
     {
-        register_activation_hook(__FILE__, array($this,'jbm_activate'));
+        if (!defined('jbm_url')) {
+            // Deprecated constants
+            define('jbm_url', plugin_dir_url(__FILE__));
+            define('jbm_path', plugin_dir_path(__FILE__));
+            define('jbm_file', __FILE__);
+        }
+        /* Activation hook fires when plugin activate */
+        register_activation_hook(jbm_file, array($this,'jbm_activate'));
+
+        /* Deactivate hook fires when plugin deactivate */
+        register_deactivation_hook(jbm_file, array($this,'jbm_deactivate'));
+
+        /* Create shortcode for client registration form */
+        add_shortcode('client_reg', array($this,'jbm_client_reg'));
+
+        /* Create shortcode for contractor registration form */
+        add_shortcode('contractor_reg', array($this,'jbm_contractor_reg'));
     }
+
+    /**
+     * Callback function of activation hook
+     *
+     * Register two roles of 'Client' and 'Contractor' when plugin activate.
+     *
+     * @since 1.0.0
+     */
 
     public function jbm_activate()
     {
+        global $wp_roles;
+        $total_role = $wp_roles->get_names();
+        $admin_role = $wp_roles->get_role('subscriber');
+        
+        if (!in_array('Client', $total_role)) {
+            add_role('client', 'Client', $admin_role->capabilities);
+        }
+        if (!in_array('Contractor', $total_role)) {
+            add_role('contractor', 'Contractor', $admin_role->capabilities);
+        }
+    }
+
+    /**
+     * Callback function of deactivation hook
+     *
+     * Remove roles of 'Client' and  'Contractor' when plugin deactivate.
+     *
+     * @since 1.0.0
+     */
+    public function jbm_deactivate()
+    {
+        global $wp_roles;
+        $total_role = $wp_roles->get_names();
+
+        if (in_array('Client', $total_role)) {
+            remove_role('client');
+        }
+        if (in_array('Contractor', $total_role)) {
+            remove_role('contractor');
+        }
+    }
+
+    /**
+     * Callback function of client registration shortcode
+     *
+     * @since 1.0.0
+     */
+    public function jbm_client_reg()
+    {
+        require_once jbm_path.'inc/client-registration.php';
+    }
+
+    /**
+     * Callback function of contractor registration shortcode
+     *
+     * @since 1.0.0
+     */
+    public function jbm_contractor_reg()
+    {
+        require_once jbm_path.'inc/contractor-regitration.php';
     }
 }
 
